ac68255598eb885494c50a80463a872b82e9fac8
pravanjan <pravanjan.niranjan.anywhere.co>
2020-05-26 15:33:39 +0530

    added changes to github package

---
+++ public/local-time.js
@@ -0,0 +1,17 @@
+var template = document.createElement('template');
+template.innerHTML =`
+  <span> </span>
+`
+
+
+class Localtime extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'}); 
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.querySelector('span').innerHTML = new Date();
+    }
+   
+}
+
+window.customElements.define('local-time',Localtime);
